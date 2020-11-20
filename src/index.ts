c6dc0770a591e6a62178ea0ee9c812615f343296
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-20 11:07:24 -0800

    Add the User on and fetch and json-server db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Simon', age: 32 });
+const user = new User({ id: 1 });
 
-user.set({ name: 'Tom' });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.set({ name: 'Simon New', age: 1000 });
+
+user.save();
