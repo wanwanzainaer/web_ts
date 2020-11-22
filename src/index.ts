c575e6df0d18c39d1420a9bbc5893ade091b6d10
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-21 16:45:22 -0800

    a sync class

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { User } from './models/User';
 const user = new User({ id: 1 });
 
 user.set({ name: 'Simon New', age: 1000 });
-
+user.events.on('change', () => {
+  console.log('change!');
+});
+user.events.trigger('change');
 user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
 
-type Callback = () => void;
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -19,42 +20,5 @@ export class User {
   set(update: UserProps): void {
     // Object.assign(this.data, update);
     this.data = { ...this.data, ...update };
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-
-    // this.events[eventName]; //Callback[] or undefined;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) return;
-
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse) => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      //put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      //post
-      axios.post(`http://localhost:3000/users/${id}`, this.data);
-    }
   }
 }
