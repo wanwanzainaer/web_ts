d6325a990e5c957493100b7ea19608a2638aa874
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-23 23:16:15 -0800

    Add the wrapup fix void

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'gogo', age: 1 });
+const user = User.buildUser({ id: 1 });
 
 console.log(user.get('name'));
 
-user.on('save', () => {
-  console.log(user);
+user.on('change', () => {
+  console.log('change');
 });
-
-user.save();
+user.fetch();
