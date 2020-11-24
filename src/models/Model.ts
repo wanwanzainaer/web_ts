ae9a2aa1914d5726db7a0fda57bac0f4bd21c85c
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-24 10:30:34 -0800

    Add short syntax

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -25,16 +25,12 @@ export class Model<T extends HasId> {
     private events: Events,
     private sync: Sync<T>
   ) {}
-  get on() {
-    return this.events.on;
-  }
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
+  // get on() {
+  //   return this.events.on;
+  // }
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
 
   set(update: T): void {
     this.attributes.set(update);
