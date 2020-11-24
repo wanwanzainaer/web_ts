9f08c5a05cc22b425c0d412e77111f8c68aaf788
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-24 12:30:44 -0800

    Add UserForm class

---
+++ src/views/UserForm.ts
@@ -0,0 +1,49 @@
+import { User } from '../models/User';
+export class UserForm {
+  constructor(public parent: Element, public model: User) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonCLick,
+      'mouseenter:h1': this.hoverOver,
+    };
+  }
+
+  hoverOver(): void {
+    console.log('H1 is hovered over');
+  }
+
+  onButtonCLick(): void {
+    console.log('hi there');
+  }
+
+  template(): string {
+    return `
+    <div>
+      <h1>User Form<h1>
+      <div>User name: ${this.model.get('name')}</div>
+      <div>User age: ${this.model.get('age')}</div>
+      <input/>
+      <button>Click Me </button>
+    </div>
+    `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+}
